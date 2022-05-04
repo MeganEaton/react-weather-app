0a0a11d7d70bc9ec47ab29aadcc049ead7584b0f
MeganEaton <101661178+MeganEaton@users.noreply.github.com>
2022-05-03 21:43:58 -0400

    Removed css weather icon folder in src and moved to public
    linked css weather icons in html head. Removed some unneeded files.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "weather-icons/css/weather-icons.css";
-import "weather-icons/css/weather-icons.min.css";
+
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
