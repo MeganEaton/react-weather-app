f8eb5c6663d4e08a8f153e80255990381316e7bf
MeganEaton <101661178+MeganEaton@users.noreply.github.com>
2022-05-10 16:04:45 -0400

    Added forecast of next 5 days using mapping
    used (0 < index && index >6) to show the days, weather icon, and max/min temp from forecast[1] to forecast[5].

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,9 +16,15 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[1]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (0 < index && index < 6) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
